e4ceac093346995f7a9cf30da2cb5bbd3a79f64c
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-28 13:00:19 +1000

    refactoring

--- common/components/User.php
+++ common/components/User.php
@@ -4,22 +4,52 @@ namespace cms\user\common\components;
 
 class User extends \yii\web\User {
 
+	/**
+	 * @var boolean
+	 */
 	private $_isAdmin;
 
+	/**
+	 * @var string
+	 */
 	private $_username;
 
+	/**
+	 * Checking that user is administrator
+	 * @return boolean
+	 */
 	public function getIsAdmin() {
-		if ($this->_isAdmin !== null) return $this->_isAdmin;
-		return $this->_isAdmin = $this->getIsGuest() ? null : (boolean) $this->getIdentity()->admin;
+		if ($this->_isAdmin !== null)
+			return $this->_isAdmin;
+
+		if ($this->getIsGuest())
+			return false;
+
+		return (boolean) $this->getIdentity()->admin;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function can($permissionName, $params = [], $allowCaching = true) {
-		return $this->getIsAdmin() ? true : parent::can($permissionName, $params, $allowCaching);
+		if ($this->getIsAdmin())
+			return true;
+
+		return parent::can($permissionName, $params, $allowCaching);
 	}
 
+	/**
+	 * Return user name
+	 * @return string
+	 */
 	public function getUsername() {
-		if ($this->_username !== null) return $this->_username;
-		return $this->_username = $this->getIsGuest() ? Yii::t('user', 'Guest') : $this->getIdentity()->getUsername();
+		if ($this->_username !== null)
+			return $this->_username;
+
+		if ($this->getIsGuest())
+			return Yii::t('user', 'Guest');
+
+		return $this->getIdentity()->getUsername();
 	}
 
 }
