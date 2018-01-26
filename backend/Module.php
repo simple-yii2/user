70ca36b82e5d022456f30390fafd1d4b484d61e1
Dmitry Khlystov <dkhlystov@gmail.com>
2018-01-26 22:07:58 +1000

    Check authManager config

--- backend/Module.php
+++ backend/Module.php
@@ -3,7 +3,9 @@
 namespace cms\user\backend;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
+use yii\rbac\BaseManager;
 
 use cms\components\BackendModule;
 use cms\user\common\components\AuthorRule;
@@ -48,6 +50,10 @@ class Module extends BackendModule {
 	protected static function cmsSecurity()
 	{
 		$auth = Yii::$app->getAuthManager();
+
+		if (!($auth instanceof BaseManager))
+			throw new InvalidConfigException('You should to configure "authManager" application component inherited from yii\rbac\BaseManager class.');
+
 		if ($auth->getRole('author') === null) {
 			//author role
 			$author = $auth->createRole('author');
